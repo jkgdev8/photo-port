c98a4dd76a619b3614263ae28b1bd44fc87309cd
Julienne Garcia <jkgdev8@gmail.com>
2022-03-08 22:22:26 -0800

    mvp

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Modal from '../Modal';
 
-
 const PhotoList = ({ category }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -123,17 +123,17 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter(photo => photo.category === category);
-  
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
   };
+
   return (
     <div>
       {isModalOpen && (
-        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />)}
-
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
